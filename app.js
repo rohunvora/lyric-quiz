697a76e19564a7bcd5d639f48327a312c3eb99e1
Rohun Vora <emailrohun@gmail.com>
2020-07-28 21:40:46 -0700

    [Midway through working lyric array]

--- app.js
+++ app.js
@@ -1,99 +1,174 @@
 let blonde = document.getElementById("blonde");
 let dieLit= document.getElementById("dieLit");
 let darkLaneDemoTapes = document.getElementById("darkLaneDemoTapes");
+let blondeAlbumURL = "https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/album.tracks.get?album_id=23915006&page=1&page_size=17&apikey="
+let fullAlbumArray = [];
+let blondeTracksURL = 'https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/album.tracks.get?album_id=23915006&page=1&page_size=17&apikey='
 let startQuiz = document.getElementById("startQuiz")
 const inputBar = document.createElement("input")
 const submitButton = document.createElement("input")
 inputBar.setAttribute("type", "text");
 submitButton.setAttribute("type", "submit");
 let blondeQuiz = document.querySelector("#quizOne");
-let blondeURL = "https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=114244840&apikey="
+let nikesNumber = 114244840;
+let dieLitQuiz = document.querySelector("#quizTwo");
+let dieLitURL = "https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=149944932&apikey="
 let rightAnswer
-let questionElement
-let previousLineElement
-let postLineElement
+let scoreBox = document.getElementById("scorebox");
+let score = document.createElement("h1");
+scoreBox.appendChild(score);
+score.innerText = 0;
+let updatedScore = 0;
+let gameActive = false;
+let lyricArray = [];
 
 
 
+    // }
 
-startQuiz.onclick = function generateQuiz() {
+// lyric quiz
+// start screen describing how the quiz works
+// function that allows users to search an artist
+// allows users to pick an artist
+// allow users to pick an album
+startQuiz.onclick = async function generateQuiz() {
+  // turn game to active
+  gameActive = true;
+  // check which album it is
   if (blonde.checked == true) {
-      let tempQuestion = getQuestion(blondeURL, blondeQuiz);
-      document.getElementById("quizOne").style.display = "block";
-      let yourScore = 0;
-      let yourScoreDisplay = document.createElement("h1")
-      blondeQuiz.appendChild(yourScoreDisplay);
-
-      for (i = 0; i < 10; i++) {
-      submitButton.onclick = function checkAnswer() {
-        yourScoreDisplay.innerText = "Your Score: " + yourScore;
-
-        if (inputBar.value == rightAnswer && yourScore === 0) {
-          yourScore = yourScore + 1;
-          document.getElementById("tempQ").style.display = "none";
-          document.getElementById("tempPrev").style.display = "none";
-          document.getElementById("tempPost").style.display = "none";
-          getQuestion(blondeURL, blondeQuiz);
-      } else {
-          console.log("Sorry the actual lyric was: " + rightAnswer)
-        }
-      };
-    }
+    lyricArray = [];
+    getAlbumArray(blondeTracksURL).then(resultArray => {
+      for (let i=0; i<resultArray.length; i++) {
+        let eachTrackID = resultArray[i];
+        let eachTrackURL = `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${eachTrackID}&apikey=`
+        fetch(eachTrackURL)
+          .then(response => {
+            return response.json()
+          })
+          .then(data => {
+            let lyricBody = data.message.body.lyrics.lyrics_body;
+            lyricArray.push(lyricBody)
+          })
+      }
+     console.log(lyricArray);
+    })
 
 
-
+    submitButton.onclick = checkAnswer
+    document.getElementById("quizOne").style.display = "block";
   } else if (dieLit.checked == true) {
+    let tempQuestion = getQuestion(dieLitURL, dieLitQuiz);
     document.getElementById("quizTwo").style.display = "block";
+
   } else if (darkLaneDemoTapes.checked == true) {
     document.getElementById("quizThree").style.display = "block";
   }
 };
 
-
-
-
-
-
-
-function getQuestion(url, quiz) {
-  fetch(url)
+// create array of track ID's
+async function getAlbumArray(url) {
+  let res = await fetch(url)
   .then(response => {
     console.log(response)
     return response.json()
   })
-  .then (data => {
-    let lyricBody = data.message.body.lyrics.lyrics_body;
-    let arrayOfLines = lyricBody.split(/\r?\n/);
-    arrayOfLines.pop();
-    arrayOfLines.pop();
-    arrayOfLines.pop();
-    let randMax = arrayOfLines.length
-    let randNum = Math.floor(Math.random() * randMax);
-    let randomIndex = arrayOfLines[randNum];
-    let previousLine = arrayOfLines[(randNum - 1)];
-    let postLine = arrayOfLines[(randNum + 1)];
-    let splitRandomIndex = randomIndex.split(" ");
-    let randTwoMax = splitRandomIndex.length;
-    let randNumTwo = Math.floor(Math.random() * randTwoMax);
-    rightAnswer = splitRandomIndex[randNumTwo];
-    splitRandomIndex[randNumTwo] = " ________ ";
-    let finalQuestion = splitRandomIndex.join(" ");
-    let questionElement = document.createElement("p");
-    questionElement.setAttribute("id", "tempQ")
-    let previousLineElement = document.createElement("p");
-    previousLineElement.setAttribute("id", "tempPrev")
-    let postLineElement = document.createElement("p");
-    postLineElement.setAttribute("id", "tempPost")
-    previousLineElement.innerText = previousLine;
-    postLineElement.innerText = postLine;
-    questionElement.innerText = finalQuestion;
-    quiz.appendChild(previousLineElement);
-    quiz.appendChild(questionElement);
-    quiz.appendChild(postLineElement);
-    quiz.appendChild(inputBar);
-    quiz.appendChild(submitButton);
-  });
+  let fullAlbumArray = [];
+    let trackList = res.message.body.track_list;
+    for (let i = 0; i < trackList.length; i++) {
+      let trackID = trackList[i].track.track_id;
+      fullAlbumArray.push(trackID);
+    }
+    return (fullAlbumArray);
+};
+
+// getAlbumArray(blondeTracksURL).then(resultArray => {
+//   console.log(resultArray);
+// });
+
+
+
+// fetch lyrics from API
+function getQuestion(array) {
+      let lyricBody = array[0];
+      let arrayOfLines = lyricBody.split(/\r?\n/);
+      arrayOfLines.pop();
+      arrayOfLines.pop();
+      arrayOfLines.pop();
+      let randMax = arrayOfLines.length
+      let randNum = Math.floor(Math.random() * randMax);
+      let randomIndex = arrayOfLines[randNum];
+      let previousLine = arrayOfLines[(randNum - 1)];
+      let postLine = arrayOfLines[(randNum + 1)];
+      let splitRandomIndex = randomIndex.split(" ");
+      let randTwoMax = splitRandomIndex.length;
+      let randNumTwo = Math.floor(Math.random() * randTwoMax);
+      rightAnswer = splitRandomIndex[randNumTwo];
+      splitRandomIndex[randNumTwo] = " ________ ";
+      let finalQuestion = splitRandomIndex.join(" ");
+      let questionElement = document.createElement("p");
+      questionElement.setAttribute("id", "tempQ")
+      let previousLineElement = document.createElement("p");
+      previousLineElement.setAttribute("id", "tempPrev")
+      let postLineElement = document.createElement("p");
+      postLineElement.setAttribute("id", "tempPost")
+      previousLineElement.innerText = previousLine;
+      postLineElement.innerText = postLine;
+      questionElement.innerText = finalQuestion;
+      quiz.appendChild(previousLineElement);
+      quiz.appendChild(questionElement);
+      quiz.appendChild(postLineElement);
+      quiz.appendChild(inputBar);
+      quiz.appendChild(submitButton);
+    }
+// checking if the user matches the lyrics
+
+function checkAnswer() {
+  if (inputBar.value == rightAnswer) {
+    console.log("You are correct")
+    scoreTracker();
+    clearQuestion();
+    loadNewQuestion();
+    getQuestion();
+  } else {
+    console.log("Sorry the actual lyric was: " + rightAnswer)
+    clearQuestion();
+    loadNewQuestion();
+    getQuestion();
+  }
+};
+// if match add to the score if not don't add to the score
+const scoreTracker = () => {
+  updatedScore += 1;
+  score.innerText = updatedScore;
 }
+
+// clear current question
+const clearQuestion = ()  => {
+    document.getElementById("tempQ").innerText = "";
+    document.getElementById("tempPrev").innerText = "";
+    document.getElementById("tempPost").innerText = "";
+    inputBar.value = "";
+
+}
+
+const loadNewQuestion = () => {
+  let newTrackID = resultarray.shift()
+}
+// move on to the next question (total questions = number of tracks)
+
+// give the user a final score out of the number of tracks
+// allow them to restart the game (clear everything out)
+
+
+
+
+
+
+
+
+
+
+
 
 //select random index from the array
 // split the array by spaces into a new array
@@ -104,5 +179,11 @@ function getQuestion(url, quiz) {
 // https://api.musixmatch.com/ws/1.1/
 // frank ocean artist id: 13928405
 // frank ocean blonde album id: 23915006
-// blonde track list fetch url "https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/album.tracks.get?album_id=23915006&page=1&page_size=17&apikey=48544aa4f2a8481d9aea9574bdd42157"
+// blonde track list fetch url "https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/album.tracks.get?album_id=23915006&page=1&page_size=17&apikey="
 // nikes track id: 114244840
+
+    // yourScore = yourScore + 1;
+
+// let yourScore = 0;
+// let yourScoreDisplay = document.createElement("h1")
+// blondeQuiz.appendChild(yourScoreDisplay);
